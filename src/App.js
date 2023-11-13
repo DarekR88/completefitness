206ed2dfffbe4b322d3f68f4e86e76ccc3ebea7c
Darek Radke <dradmclad@gmail.com>
2023-11-13 00:21:23 -0600

    necessary state and subsequent functions setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React, { useState, useRef, useEffect } from "react";
 import "./Global.scss";
 
 const App = () => {
+
+  const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
+  const [toolbarShow, setToolbarShow] = useState(true);
+  const [backdropShow, setBackdropShow] = useState(false);
+  
+  const toggleBackdrop = () => {
+    setBackdropShow(!backdropShow)
+  }
+
+  const toggleToolbar = () => {
+    setToolbarShow(!toolbarShow)
+  }
+
+  const toggleSideDrawer = () => {
+    setSideDrawerOpen(!sideDrawerOpen)
+  }
+
   return (
     <div className="App">
       <p>Hello World!</p>
