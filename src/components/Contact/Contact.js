ff3059f41f1029ea0a7e6a512c1cc89c0a4b2d59
Darek Radke <dradmclad@gmail.com>
2023-11-15 22:40:22 -0600

    css for mobile view rough draft

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -28,25 +28,64 @@ const Contact = (props) => {
   return (
     <div className="contact-wrapper">
       <div className="contact-title">Contact</div>
+      <p className="contact-blurb">Operators are standing by 24/7</p>
+      <p className="contact-blurb">Call Now!</p>
+      <p className="contact-blurb">(420) 420-6969</p>
+      <p className="contact-blurb">OR</p>
+      <p className="contact-blurb">Email: buttshigger@Ignitionfuckness.mail</p>
+      <p className="contact-blurb long">
+        Taking the first step towards a better you can be very difficult and we
+        are here to facilitate your transformation journey in a way that
+        maximizes your results.
+      </p>
       <form className="contact-form" id="form" onSubmit={sendEmail}>
         <div class="field">
           <label className="contact-label" for="to_name"></label>
-          <input className="contact-input name" placeholder="Name" type="text" name="to_name" id="to_name" />
+          <input
+            className="contact-input name"
+            placeholder="Name"
+            type="text"
+            name="to_name"
+            id="to_name"
+          />
         </div>
         <div class="field">
           <label className="contact-label" for="from_name"></label>
-          <input className="contact-input phone" placeholder="Phone" type="text" name="from_name" id="from_name" />
+          <input
+            className="contact-input phone"
+            placeholder="Phone"
+            type="text"
+            name="from_name"
+            id="from_name"
+          />
         </div>
         <div class="field">
           <label className="contact-label" for="reply_to"></label>
-          <input className="contact-input email" placeholder="Email" type="text" name="reply_to" id="reply_to" />
+          <input
+            className="contact-input email"
+            placeholder="Email"
+            type="text"
+            name="reply_to"
+            id="reply_to"
+          />
         </div>
         <div class="field">
           <label className="contact-label" for="message"></label>
-          <input className="contact-input message" placeholder="Message" type="text" name="message" id="message" />
+          <input
+            className="contact-input message"
+            placeholder="Message"
+            type="text"
+            name="message"
+            id="message"
+          />
         </div>
 
-        <input type="submit" id="button" value="Send Email" />
+        <input
+          className="contact-send"
+          type="submit"
+          id="button"
+          value="Send Message"
+        />
       </form>
     </div>
   );
